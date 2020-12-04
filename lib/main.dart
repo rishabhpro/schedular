ff40cbdfdd3dd43015b84ebfd37382d59bf4e433
Anushka Srivastava <srivastavarishi29@gmail.com>
2020-12-04 19:11:12 +0530

    sizebox config

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,10 @@ import 'package:schedular/screens/Teacher_signUp.dart';
 import 'package:schedular/screens/Student_signup.dart';
 import 'package:schedular/screens/entry_page.dart';
 import 'package:schedular/screens/login_page.dart';
+import 'package:schedular/screens/Teacher DashBoard.dart';
 
 void main() {
   runApp(MaterialApp(
-    home:EntryPage(),
+    home: StudentDashBoard(),
   ));
 }
