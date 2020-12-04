0b8f15489281ea04cd8a3d65126a400d95f8a992
sakshamyadav09 <saksham.cs21@iilmcet.ac.in>
2020-12-04 15:40:16 +0530

    Added login page and entry page

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:schedular/screens/LandingPage.dart';
 import 'package:schedular/screens/Teacher_signUp.dart';
 import 'package:schedular/screens/Student_signup.dart';
+import 'package:schedular/screens/entry_page.dart';
+import 'package:schedular/screens/login_page.dart';
 
 void main() {
   runApp(MaterialApp(
-    home:Ssp(),
+    home:LoginPage(),
   ));
 }
-
