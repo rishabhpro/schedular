c4b3d96b3f4323ce5680f8a0d448206b55ed3a5d
sakshamyadav09 <saksham.cs21@iilmcet.ac.in>
2020-12-04 15:46:44 +0530

    added entry_page & login_page

---
+++ lib/screens/entry_page.dart
@@ -0,0 +1,75 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class EntryPage extends StatefulWidget {
+  @override
+  _EntryPageState createState() => _EntryPageState();
+}
+
+class _EntryPageState extends State<EntryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: SafeArea(
+        child: Container(
+        decoration: BoxDecoration(
+        image: DecorationImage(
+        image: AssetImage('assets/bglogin.png'),
+        fit: BoxFit.cover,
+    ),
+    ),
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(0.0, 250.0, 200.42, 0.0),
+            child: Column(
+              children: <Widget>[
+                RaisedButton.icon(onPressed: (){}, icon: Icon(
+                  Icons.account_circle_outlined,
+                  color: Colors.red[800],),
+                    label: Text('Login',
+                  style: TextStyle(
+                  color: Colors.white,
+                  backgroundColor: Colors.blueGrey,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 60.0,
+                  ),
+                ),
+                ),
+                Text('If you have already created your account with us '
+                    'then click on Login button',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15.0,
+                ),
+                ),
+                SizedBox(height: 270.0),
+            Row(children: <Widget>[
+              Text('Not a member => ',
+              style:TextStyle(
+                color: Colors.blueGrey,
+                fontSize: 14.2,
+              ),
+              ),
+                FlatButton(
+                  color: Colors.blueGrey,
+                  textColor: Colors.white,
+                 // disabledColor: Colors.grey,
+                //  disabledTextColor: Colors.black,
+                  padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                  //splashColor: Colors.blueAccent,
+                  onPressed: () {},
+                  child: Text(
+                    "Regiser!",
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ),
+            ],
+            ),
+              ],
+            ),
+          ),
+      ),
+    ),
+    );
+  }
+}
---
+++ lib/screens/login_page.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/bgtwo.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+         // crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(padding: EdgeInsets.fromLTRB(0.0, 310.0, 0.0, 0.0),),
+            TextField(
+              style: TextStyle(fontSize: 20.0,color: Colors.black54),
+              decoration:InputDecoration(
+                filled: true,
+                fillColor: Colors.grey[200],
+                hintText: 'Enter Email',
+                contentPadding:EdgeInsets.all(15),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(500),
+                ),
+              ) ,
+            ),
+            SizedBox(height: 15.0),
+            TextField(
+              // controller: _text,
+              style: TextStyle(fontSize: 20.0 , color: Colors.black54),
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.grey[200],
+                hintText: 'Enter Password',
+                //  errorText: _validate ? 'Value Can\'t Be Empty' : null,
+                contentPadding: const EdgeInsets.all(15),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(500),
+                ),
+              ),
+            ),
+            SizedBox(height:10.0 ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              FlatButton(
+               // color: Colors.blueGrey,
+                textColor: Colors.blueGrey,
+                // disabledColor: Colors.grey,
+                //  disabledTextColor: Colors.black,
+               // padding: EdgeInsets.fromLTRB(5.0, 10.0, 200.0, 0.0),
+                //splashColor: Colors.blueAccent,
+                onPressed: () {},
+                child: Text(
+                  "Forget Password",
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+              SizedBox(width: 80.0),
+              FlatButton(
+                 color: Colors.red,
+                textColor: Colors.black,
+                // disabledColor: Colors.grey,
+                //  disabledTextColor: Colors.black,
+               // padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                //splashColor: Colors.blueAccent,
+                onPressed: () {},
+                child: Text(
+                  "Login",
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+            ],
+            ),
+            SizedBox(height: 200.0),
+            Row(
+              children: <Widget>[
+                SizedBox(width: 10.0),
+                Text('Not a member => ',
+                  style:TextStyle(
+                    color: Colors.blueGrey,
+                    fontSize: 14.2,
+                  ),
+                ),
+                FlatButton(
+                  color: Colors.blueGrey,
+                  textColor: Colors.white,
+                  // disabledColor: Colors.grey,
+                  //  disabledTextColor: Colors.black,
+                 // padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                  //splashColor: Colors.blueAccent,
+                  onPressed: () {},
+                  child: Text(
+                    "Regiser!",
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        ),
+      ),
+    );
+  }
+}
