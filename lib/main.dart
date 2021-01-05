6c757a2de66c1baf452d2c31b1933af85535b6f4
Anushka Srivastava <srivastavarishi29@gmail.com>
2021-01-05 17:42:56 +0530

    Added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:schedular/screens/LandingPage.dart';
 import 'package:schedular/screens/Teacher_signUp.dart';
-import 'package:schedular/screens/Student_signup.dart';
-import 'package:schedular/screens/entry_page.dart';
-import 'package:schedular/screens/login_page.dart';
-import 'package:schedular/screens/Teacher DashBoard.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: LoginPage(),
+    home: Tsp(),
   ));
 }
