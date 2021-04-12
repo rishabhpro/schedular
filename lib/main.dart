03fc052d57c2e6f57a84d42c6ad703193fa9c2ff
sakshamyadav09 <saksham.cs21@iilmcet.ac.in>
2021-04-12 16:10:05 +0530

    updated the initial route

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,21 @@ import 'package:flutter/material.dart';
 import 'file:///D:/schedular/schedular/lib/screens/Teacher_signUp.dart';
 import 'file:///D:/schedular/schedular/lib/screens/Student_signup.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:schedular/screens/LandingPage.dart';
+import 'package:schedular/screens/Student%20DashBoard.dart';
 import 'package:schedular/screens/entry_page.dart';
 import 'package:schedular/screens/login_page.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Ssp(),
+      initialRoute: '/entrypage',
+      routes: {
+      '/entrypage': (context) => EntryPage(),
+      '/studentdashboard': (context) => StudentDashBoard(),
+      '/login': (context) => LoginPage(),
+      '/teachersignup' : (context) => Tsp(),
+       '/studentsignup': (context) => Ssp(),
+       '/lp': (context) => LandingPage(),}
+   // home: EntryPage(),
   ));
 }
