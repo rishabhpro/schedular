730bd904d25a47878532b0c715bf0b443654d6a9
sakshamyadav09 <saksham.cs21@iilmcet.ac.in>
2021-04-12 14:56:13 +0530

    Added Authentication Service

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'file:///D:/schedular/schedular/lib/screens/Teacher_signUp.dart';
 import 'file:///D:/schedular/schedular/lib/screens/Student_signup.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:schedular/screens/entry_page.dart';
 import 'package:schedular/screens/login_page.dart';
 
 void main() {
